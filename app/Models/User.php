56da925e004d987e5a222ecb51e78a37b4af9e8b
Bruno <bruno.araujo@servidor.uepb.edu.br>
2022-04-28 23:35:49 -0300

    Adicionados relacionamentos entre os modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    public function pontos()
+    {
+        return $this->hasMany(Ponto::class);
+    }
+
+    /**
+     * Get the user's most recent point.
+     */
+    public function ultimoPonto()
+    {
+        return $this->hasOne(Ponto::class)->latestOfMany();
+    }
 }
