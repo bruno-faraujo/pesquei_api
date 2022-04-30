78aa139ab8a18068db8201bd4a7201045e4ec48f
Bruno <bruno.araujo@servidor.uepb.edu.br>
2022-04-30 09:20:42 -0300

    Criação da classe

---
+++ app/Http/Controllers/FotoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FotoController extends Controller
+{
+    public function getFotos()
+    {
+        // Terminar...
+    }
+
+    public function getFoto($id)
+    {
+        // Terminar...
+    }
+
+    public function novaFoto(Request $request)
+    {
+        // Terminar...
+    }
+
+    public function updateFoto(Request $request, $id)
+    {
+        // Terminar...
+    }
+
+    public function deleteFoto($id)
+    {
+        // Terminar...
+    }
+}
