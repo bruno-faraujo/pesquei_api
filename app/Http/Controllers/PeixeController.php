bbe7dc863da24baa2670afbefe6c27ef397e97b7
Bruno <bruno.araujo@servidor.uepb.edu.br>
2022-04-30 09:20:09 -0300

    Definidos os nomes dos métodos

--- app/Http/Controllers/PeixeController.php
+++ app/Http/Controllers/PeixeController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class PeixeController extends Controller
 {
-    //
+    public function getPeixes()
+    {
+        // Terminar...
+    }
+
+    public function getPeixe($id)
+    {
+        // Terminar...
+    }
+
+    public function novoPeixe(Request $request)
+    {
+        // Terminar...
+    }
+
+    public function updatePeixe(Request $request, $id)
+    {
+        // Terminar...
+    }
+
+    public function deletePeixe($id)
+    {
+        // Terminar...
+    }
+
 }
