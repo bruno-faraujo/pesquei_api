4065e648c8d2bd5bed922d95ae56826a037944ab
Bruno <bruno.araujo@servidor.uepb.edu.br>
2022-04-29 22:23:46 -0300

    Criadas as operações CRUD do Ponto

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -2,9 +2,112 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ponto;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class PontoController extends Controller
 {
-    //
+    /**
+     * Retorna todos os pontos de um determinado usuário
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getPontos()
+    {
+        $pontos = auth()->user()->pontos()->get();
+        return response()->json($pontos);
+    }
+
+    /**
+     * Retorna um ponto do usuário a partir do id
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getPonto($id)
+    {
+        try {
+            $ponto = auth()->user()->pontos()->findOrFail($id);
+        }
+        catch (ModelNotFoundException)
+        {
+            return response()->json(['error' => 'Ponto inválido'], 406);
+        }
+        return response()->json($ponto);
+
+    }
+
+    /**
+     * Cria um novo ponto
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function novoPonto(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|string',
+            'latitude' => 'required|string',
+            'longitude' => 'required|string'
+        ]);
+
+        /*
+         * Checa se já existe um ponto com as mesmas coordenadas
+         */
+        try {
+            auth()->user()->pontos()->where('latitude', $request->latitude)->where('longitude', $request->longitude)->firstOrFail();
+                }
+                catch (ModelNotFoundException)
+                {
+                    $ponto = new Ponto();
+                    $ponto->nome = $request->nome;
+                    $ponto->latitude = $request->latitude;
+                    $ponto->longitude = $request->longitude;
+                    auth()->user()->pontos()->save($ponto);
+
+                    return response()->json($ponto);
+                }
+
+                return response()->json(['error' => 'Já existe um ponto cadastrado com as coordenadas informadas'], 406);
+    }
+
+    /**
+     * Atualiza as informações de um ponto existente
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function updatePonto(Request $request, $id)
+    {
+        try {
+            $ponto = auth()->user()->pontos()->findOrFail($id);
+        }
+        catch (ModelNotFoundException)
+        {
+            return response()->json(['error' => 'Ponto inválido'], 406);
+        }
+        $ponto->update($request->all());
+
+        return response()->json($ponto);
+    }
+
+    /**
+     * Localiza e apaga um ponto a partir do id
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deletePonto($id)
+    {
+        try {
+            $ponto = auth()->user()->pontos()->findOrFail($id);
+        }
+        catch (ModelNotFoundException)
+        {
+
+            return response()->json(['error' => 'Ponto inválido'], 406);
+        }
+
+        $ponto->delete();
+
+        return response()->json(['message' => 'O ponto foi apagado com sucesso'], 200);
+
+    }
 }
