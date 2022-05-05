8c0eb49f0d6628e5738ee4dc1364f322f8f01adc
Bruno <bruno.araujo@servidor.uepb.edu.br>
2022-05-05 20:55:53 -0300

    Remoção do model Foto e refatoração do model Pescado

--- app/Models/Pescado.php
+++ app/Models/Pescado.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Pescado extends Model
+class Pescado extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->fit('contain', 300, 300)
+            ->optimize();
+
+        $this->addMediaConversion('foto')
+            ->fit('contain', 1000, 1000)
+            ->optimize();
+    }
 
     public function ponto()
     {
@@ -17,11 +31,6 @@ class Pescado extends Model
     public function peixe()
     {
         return $this->belongsTo(Peixe::class);
-    }
-
-    public function fotos()
-    {
-        return $this->hasMany(Foto::class);
     }
 
 }
