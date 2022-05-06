e1d664487a7a4826e285bbda5abd9e864faa30c3
Bruno <bruno.araujo@servidor.uepb.edu.br>
2022-05-06 18:19:56 -0300

    Correção em uma rota

--- routes/api.php
+++ routes/api.php
@@ -47,8 +47,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('ponto/{ponto_id}/pescado/{pescado_id}/fotos', [FotoController::class, 'getFotos']);
     Route::get('ponto/{ponto_id}/pescado/{pescado_id}/foto/{media_id}', [FotoController::class, 'getFoto']);
-    Route::post('ponto/{ponto_id}/pescado/{pescado_id}/foto_delete/{foto_id}', [FotoController::class, 'deleteFoto']);
-    Route::post('ponto/{ponto_id}/pescado/{pescado_id}/foto_update/{foto_id}', [FotoController::class, 'updateFoto']);
+    Route::get('ponto/{ponto_id}/pescado/{pescado_id}/foto_delete/{media_id}', [FotoController::class, 'deleteFoto']);
+    Route::post('ponto/{ponto_id}/pescado/{pescado_id}/foto_update/{media_id}', [FotoController::class, 'updateFoto']);
     Route::post('nova_foto', [FotoController::class, 'novaFoto']);
 
 });
